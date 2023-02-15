2261af4665064c85fed1e5248b8a0d28a147dda4
Demond Harris <Dae@demonds-mbp.myfiosgateway.com>
2023-02-14 19:00:29 -0500

    functions updates

---
+++ functionsApril.js
@@ -0,0 +1,19 @@
+//FunctionAverage
+//Part 1
+
+let num1 = 6
+let num2 = 2
+function average(num1, num2){
+return (num1 + num2)
+console.log(average(num1, num2))
+}
+
+//Part 2
+//let a = firstNumber
+//let b = secondNumber
+//let c = thirdNumber
+//function average(a,b,c){
+  //  console.log(a + b + c) /2
+//}
+
+//Defaults
