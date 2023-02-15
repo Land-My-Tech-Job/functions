995d79724bdc033f37bc26d35d18de9f65518af2
Demond Harris <Dae@demonds-mbp.myfiosgateway.com>
2023-02-15 13:01:36 -0500

    functionsUpdates

--- functionsApril.js
+++ functionsApril.js
@@ -1,19 +1,66 @@
 //FunctionAverage
 //Part 1
+//let num1 = 6
+//let num2 = 2
+//function average(num1, num2){
+//return (num1 + num2) /2
+//}
+//console.log(average(num1, num2))
 
-let num1 = 6
-let num2 = 2
-function average(num1, num2){
-return (num1 + num2)
-console.log(average(num1, num2))
-}
 
 //Part 2
-//let a = firstNumber
-//let b = secondNumber
-//let c = thirdNumber
-//function average(a,b,c){
-  //  console.log(a + b + c) /2
+//let a = 6
+//let b = 8
+//let c = 4
+//function average(a,b,c) {
+//return (a + b + c) /3
 //}
+//console.log(average(a, b, c))
 
-//Defaults
+
+
+//Defaults(a)
+//function cube(n = 5) {
+//return n * n * n
+//}
+//console.log(cube(10))
+//console.log(cube())
+
+//(b)
+//function defaultName() {
+  //return 'My name is April'
+//}
+//let name = 
+//console.log(name) 
+
+//Divide(a)
+
+//let a = 888.786
+//let b = 444.789
+//let c = a / b 
+//function divideTwo(a, b){
+//return a / b
+//}
+//console.log(c)
+
+//Divide (b)
+//let a = 888.786
+//let b = 444.789
+//let c = a / b 
+//function divideTwoRound(a, b){
+//return a / b
+//}
+//console.log(Math.round(c))
+
+//Divide (c)
+//let a = 4
+//let b = 0
+//let Infinity = a / b
+//function divideByZero(a, b){
+//}
+//console.log(Infinity)
+
+
+
+
+
